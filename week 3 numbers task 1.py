136b0682be2d423c52082acfc7d0c1a064e91ca6
hbashir29 <43992218+hbashir29@users.noreply.github.com>
2018-10-09 14:54:28 +0100

    Add files via upload

---
+++ week 3 numbers task 1.py
@@ -0,0 +1,5 @@
+mynumber1 = int(input("please enter 1st number"))
+mynumber2 = int(input("please enter 2nd number"))
+sum = float(mynumber1) + float(mynumber2)
+print('{0} + {1} = {2}'.format(mynumber1,mynumber2,sum))
+
